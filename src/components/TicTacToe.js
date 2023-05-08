53e7ff831c051c20e82864c03cbc5095cc88730a
Isaac <isaachernandez8@gmail.com>
2023-05-08 10:32:53 -0700

    can input names, set playernumbers, reset board, reset page

--- src/components/TicTacToe.js
+++ src/components/TicTacToe.js
@@ -10,6 +10,7 @@ const TicTacToe = () => {
   ]);
   const players = ["x", "o"]
   const [currentPlayer, setCurrentPlayer] = useState(players[Math.round(Math.random())]);
+  const [playerSelect, setPlayerSelect] = useState(true)
   const [gameOver, setGameOver] = useState(true);
   const [P1Name, setP1Name] = useState("1P")
   const [P2Name, setP2Name] = useState("2P")
@@ -27,7 +28,13 @@ const TicTacToe = () => {
   }
 
   function twoPlayer() {
-
+    if (!playerNumbers) {
+      setPlayerNumbers(2)
+              document.getElementById("1Player").style.display = "none"
+              document.getElementById("2Player").style.display = "none"
+              document.getElementById("P1-name-input").style.display = "inline"
+              document.getElementById("P2-name-input").style.display = "inline"
+              }
   }
 
   function resetBoard() {
@@ -55,6 +62,17 @@ const TicTacToe = () => {
       setBoard(boardCopy);
       switchPlayer();
     }
+  }
+
+  function computerTurn() {
+    let potentialMoves = []
+    for (let i = 0; i < board.length; i++) {
+      for (let j = 0; j < board[i].length; j++) {
+        if (!gameState.board[i][j]) {
+          potentialMoves.push([j, i])}}
+    } 
+    let move = potentialMoves[Math.floor(Math.random() * potentialMoves.length)]
+    inputTurn(move[0], move[1])
   }
 
   function checkAcross() {
@@ -136,7 +154,7 @@ const TicTacToe = () => {
         &lt;-
       </div>
       <div className="row">
-        <div>
+        <div className="column">
           <div className="title">{message}</div>
           <div className={`board ${gameOver ? null : "clickable"}`}>
             <div
@@ -249,22 +267,42 @@ const TicTacToe = () => {
             </div>
           </div>
           <div id="display-names" className="row">
-            <span className='player-option clickable highlight' id="1Player" onClick={onePlayer}>{P1Name}</span>
-            <input id="P1-name-input" className="player-option" value={P1Name} onChange={(e) => setP1Name(e.value)}></input>
-            <span className='player-option clickable highlight' id="2Player" onClick={() => {setPlayerNumbers(2)}}>{P2Name}</span>
-            <input id="P2-name-input" className="player-option" value={P2Name} onChange={(e) => setP2Name(e.value)}></input>
+            <span className={`player-option ${gameOver && !playerNumbers ? "clickable highlight" : null}`} id="1Player" onClick={onePlayer}>{P1Name}</span>
+            <input id="P1-name-input" className="player-option" value={P1Name} onChange={(e) => setP1Name(e.target.value)}></input>
+            <span className={`player-option ${gameOver && !playerNumbers ? "clickable highlight" : null}`} id="2Player" onClick={twoPlayer}>{P2Name}</span>
+            <input id="P2-name-input" className="player-option" value={P2Name} onChange={(e) => setP2Name(e.target.value)}></input>
           </div>
         </div>
         <div>
-        <div className="highlight clickable button" onClick={resetBoard}>
-          Reset
-        </div>
-        <div className="highlight clickable button" onClick={() => navigate('/tictactoe')}>
+        <div className="highlight clickable button" onClick={() => {
+          resetBoard()
+          setGameOver(true)
+          setPlayerNumbers(0)
+          setPlayerSelect(true)
+          setP1Name("1P")
+          setP2Name("2P")
+          document.getElementById("1Player").style.display = "inline"
+          document.getElementById("2Player").style.display = "inline"
+            document.getElementById("P1-name-input").style.display = "none"
+            document.getElementById("P2-name-input").style.display = "none"
+            setMessage("Number of Players?")
+        }}>
           Players
         </div>
-        <div className="highlight clickable button">
+        { playerNumbers && !playerSelect ? <div className="highlight clickable button" onClick={resetBoard}>
+          Reset
+        </div> : null}
+        {playerNumbers && playerSelect ? <div className="highlight clickable button"
+        onClick={() => {
+          setGameOver(false)
+          setPlayerSelect(false)
+          document.getElementById("1Player").style.display = "inline"
+          document.getElementById("2Player").style.display = "inline"
+            document.getElementById("P1-name-input").style.display = "none"
+            document.getElementById("P2-name-input").style.display = "none"
+        }}>
           Submit
-        </div>
+        </div> : null}
         </div>
       </div>
     </div>
--- src/game_logic/tic-tac-toe.js
+++ src/game_logic/tic-tac-toe.js
@@ -1,87 +1,124 @@
-let gameState = {
-    players: ['x', 'o'],
-    board: [
-      [null, null, null],
-      [null, null, null],
-      [null, null, null]
-    ],
-    playerNames: ["", ""]
-  }
+export function onePlayer() {
+  if (!playerNumbers) {
+  setPlayerNumbers(1)
+          document.getElementById("1Player").style.display = "none"
+          document.getElementById("P1-name-input").style.display = "inline"
+          setP2Name("COMPUTER")
+          }
+}
 
-let currentPlayer = gameState.players[Math.round(Math.random())]
+export function twoPlayer() {
+  if (!playerNumbers) {
+    setPlayerNumbers(2)
+            document.getElementById("1Player").style.display = "none"
+            document.getElementById("2Player").style.display = "none"
+            document.getElementById("P1-name-input").style.display = "inline"
+            document.getElementById("P2-name-input").style.display = "inline"
+            }
+}
 
 export function resetBoard() {
-    gameState = {
-        players: ['x', 'o'],
-        board: [
-          [null, null, null],
-          [null, null, null],
-          [null, null, null]
-        ],
-        playerNames: ["", ""]}
-        return gameState.board
+  setBoard([
+    [null, null, null],
+    [null, null, null],
+    [null, null, null],
+  ]);
+  setGameOver(false);
+  setMessage("Start");
 }
 
 function switchPlayer() {
-    if (currentPlayer === "x") {currentPlayer = "o"}
-    else if (currentPlayer === "o") {currentPlayer = "x"}
+  if (currentPlayer === "x") {
+    setCurrentPlayer("o");
+  } else if (currentPlayer === "o") {
+    setCurrentPlayer("x");
+  }
 }
 
 export function inputTurn(x, y) {
-  if (!gameState.board[y][x]) {
-    gameState.board[y][x] = currentPlayer
-    switchPlayer()}
-    return gameState.board
+  if (!board[y][x]) {
+    let boardCopy = [...board];
+    boardCopy[y][x] = currentPlayer;
+    setBoard(boardCopy);
+    switchPlayer();
+  }
 }
 
 export function computerTurn() {
   let potentialMoves = []
-  for (let i = 0; i < gameState.board.length; i++) {
-    for (let j = 0; j < gameState.board[i].length; j++) {
+  for (let i = 0; i < board.length; i++) {
+    for (let j = 0; j < board[i].length; j++) {
       if (!gameState.board[i][j]) {
         potentialMoves.push([j, i])}}
   } 
   let move = potentialMoves[Math.floor(Math.random() * potentialMoves.length)]
   inputTurn(move[0], move[1])
-  updateBoard()
-  return gameState
 }
 
 function checkAcross() {
-  for (let i = 0; i < gameState.board.length; i++) {
-    let boardRow = gameState.board[i]
-    if (boardRow[0] === boardRow[1] && boardRow[1] === boardRow[2] && boardRow[0]) {
-      return `${boardRow[0].toUpperCase()} Wins!`
+  for (let i = 0; i < board.length; i++) {
+    let boardRow = board[i];
+    if (
+      boardRow[0] === boardRow[1] &&
+      boardRow[1] === boardRow[2] &&
+      boardRow[0]
+    ) {
+      return `${boardRow[0].toUpperCase()} Wins!`;
     }
   }
 }
 
 function checkVertical() {
-  for (let i = 0; i < gameState.board.length; i++) {
-    if (gameState.board[0][i] === gameState.board[1][i] && gameState.board[1][i] === gameState.board[2][i] && gameState.board[0][i]) {
-      return `${gameState.board[0][i].toUpperCase()} Wins!`
+  for (let i = 0; i < board.length; i++) {
+    if (
+      board[0][i] === board[1][i] &&
+      board[1][i] === board[2][i] &&
+      board[0][i]
+    ) {
+      return `${board[0][i].toUpperCase()} Wins!`;
     }
   }
 }
 
 function checkDiagonal() {
-  if (gameState.board[0][0] === gameState.board[1][1] && gameState.board[1][1] === gameState.board[2][2] && gameState.board[0][0]) {
-    return `${gameState.board[0][0].toUpperCase()} Wins!`
-  } else if (gameState.board[0][2] === gameState.board[1][1] && gameState.board[1][1] === gameState.board[2][0] && gameState.board[1][1]) {
-    return `${gameState.board[0][2].toUpperCase()} Wins!`
+  if (
+    board[0][0] === board[1][1] &&
+    board[1][1] === board[2][2] &&
+    board[0][0]
+  ) {
+    return `${board[0][0].toUpperCase()} Wins!`;
+  } else if (
+    board[0][2] === board[1][1] &&
+    board[1][1] === board[2][0] &&
+    board[1][1]
+  ) {
+    return `${board[0][2].toUpperCase()} Wins!`;
   }
 }
 
 function checkTie() {
-  for (let i = 0; i < gameState.board.length; i++) {
-    for (let j = 0; j < gameState.board[i].length; j++) {
-      if (!gameState.board[i][j]) {return}}}
-    return "Tie"
+  for (let i = 0; i < board.length; i++) {
+    for (let j = 0; j < board[i].length; j++) {
+      if (!board[i][j]) {
+        return;
+      }
     }
+  }
+  return "Tie";
+}
 
 export function checkWin() {
-  if (checkAcross()){return checkAcross()}
-  else if (checkVertical()){return checkVertical()}
-  else if (checkDiagonal()) {return checkDiagonal()}
-  else if (checkTie()) {return checkTie()}
+  if (checkAcross()) {
+    setMessage(checkAcross())
+    setGameOver(true);
+  } else if (checkVertical()) {
+    setMessage(checkVertical())
+    setGameOver(true);
+  } else if (checkDiagonal()) {
+    setMessage(checkDiagonal())
+    setGameOver(true);
+  } else if (checkTie()) {
+    setMessage(checkTie())
+    setGameOver(true);
+  }
 }
