78a76c66abc9abfe77e9c4a208a0f0d262a68d60
Isaac <isaachernandez8@gmail.com>
2023-05-10 23:08:15 -0700

    word is randomized, game checks for win, functionality mostly done

--- src/components/Hangman.js
+++ src/components/Hangman.js
@@ -1,18 +1,58 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import {} from "../game_logic/hangman";
+import axios from "axios";
 
 const Hangman = () => {
   const navigate = useNavigate();
 const alphabet = Array.from("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
 const [gameState, setGameState] = useState({
     word: "word",
-    definition: "",
+    definition: "definition",
     tries: 10,
     message: "Guesses Remaining: 10",
     gameOver: false,
     guesses: []
 })
+
+    async function getWord() {
+            const options = {
+            method: 'GET',
+            url: 'https://wordsapiv1.p.rapidapi.com/words/',
+            params: {random: 'true'},
+            headers: {
+              'X-RapidAPI-Key': '99b1ff4fcemsh364b18358da03dcp1fb987jsn4f1c5096428f',
+              'X-RapidAPI-Host': 'wordsapiv1.p.rapidapi.com'
+            }
+          };
+          
+          try {
+              const response = await axios.request(options);
+              return(response.data);
+          } catch (error) {
+              console.error(error);
+          }
+    }
+
+    function checkWin(gameState) {
+        for (let i = 0; i < gameState.word.length; i++) {
+            if (!alphabet.includes(gameState.word[i].toUpperCase())) {continue}
+            if (!gameState.guesses.includes(gameState.word[i].toUpperCase())) {
+                return false
+            }
+        } return true
+    }
+
+    useEffect(() => {
+        const getGameWord = async () => {
+            let data = await getWord();
+            while (!data.results[0]) {data = await getWord();}
+            let gameStateCopy = {...gameState}
+            gameStateCopy.word = data.word
+            gameStateCopy.definition = data.results[0].definition
+            setGameState(gameStateCopy);
+          };
+          getGameWord()
+    }, [])
 
   return (
     <div id="hangman">
@@ -31,7 +71,7 @@ const [gameState, setGameState] = useState({
                 <div id="rafter" className={gameState.tries >= 8 ? null : "active"}></div>
                 <div className="row" id="between">
                   <div id="hanged-man" className="row">
-                    <div id="left-arm"></div>
+                    <div id="left-arm" style={gameState.tries >= 3 ? {borderColor:"black"} : null}></div>
                     <div id="center" className="column">
                       <div id="rope" className={gameState.tries >= 7 ? null : "active"}></div>
                       <div id="head" className={gameState.tries >= 6 ? null : "active"}></div>
@@ -41,18 +81,19 @@ const [gameState, setGameState] = useState({
                         <div id="right-leg" className={gameState.tries >= 1 ? null : "active"}></div>
                       </div>
                     </div>
-                    <div id="right-arm"></div>
+                    <div id="right-arm" style={gameState.tries >= 4 ? {borderColor:"black"} : null}></div>
                   </div>
                   <div id="pole" className={gameState.tries >= 9 ? null : "active"}></div>
                 </div>
                 <div id="floor" className={gameState.tries >= 10 ? null : "active"}></div>
               </div>
               <div className="row" style={{justifyContent:"space-between"}}>
-              {Array.from(gameState.word.toUpperCase()).map((letter) => {
+              {gameState.word.length ? Array.from(gameState.word.toUpperCase()).map((letter) => {
                 return <span className="letter" style={{textDecoration:"underline", margin:"5px"}}
-                >{gameState.guesses.includes(letter) ? letter : <span>&nbsp;</span>}</span>
-              })}</div>
+                >{alphabet.includes(letter) ? gameState.guesses.includes(letter) ? letter : <span>&nbsp;</span> : letter}</span>
+              }) : null}</div>
           </div>
+          <div className="column">
           <div className="row" style={{width:"65% "}}>
           <div id="alphabet">
           {alphabet.map((letter) => {
@@ -60,28 +101,39 @@ const [gameState, setGameState] = useState({
                 if (!gameState.gameOver && !gameState.guesses.includes(letter)) {
                     let gameStateCopy = {...gameState}
                     gameStateCopy.guesses.push(letter)
-                    gameStateCopy.tries --
-                    gameStateCopy.message = `Guesses Remaining: ${gameStateCopy.tries}`
+                    if (checkWin(gameStateCopy)) {gameStateCopy.gameOver = true
+                    gameStateCopy.message = "You Win!"}
+                    if (!gameState.word.toUpperCase().includes(letter)) {gameStateCopy.tries --
+                    gameStateCopy.message = `Guesses Remaining: ${gameStateCopy.tries}`}
                     if (!gameStateCopy.tries) {gameStateCopy.gameOver = true
                     gameStateCopy.message = "Game Over"}
                     setGameState(gameStateCopy)
                 }
             }}
-            className={`letter smaller-button ${gameState.guesses.includes(letter) ? "active" : "highlight clickable"}`}>{letter}</span>
+            className={`letter smaller-button ${gameState.guesses.includes(letter) ? "active" : gameState.gameOver ? null : `highlight clickable`}`}>{letter}</span>
           })}</div>
           <div className="column">
-          <div className="button highlight clickable" onClick={() => {
+          <div className="button highlight clickable" onClick={async () => {
+            let gameStateCopy = {...gameState}
+            gameStateCopy.message = "Searching..."
+            setGameState(gameStateCopy)
+            let data = await getWord();
+            while (!data.results) {data = await getWord();}
+            document.getElementById("hint").style.display = "none"
             setGameState({
-                word: "",
-                definition: "",
+                word: data.word,
+                definition: data.results[0].definition,
                 tries: 10,
                 message: "Guesses Remaining: 10",
                 gameOver: false,
                 guesses: []
             })
           }}>New Word</div>
-          <div className="button highlight clickable">Hint</div>
+          <div className="button highlight clickable"
+          onClick={() => {document.getElementById("hint").style.display = "inline"}}>Hint</div>
           </div>
+          </div>
+          <div id="hint">{gameState.definition}</div>
           </div>
       </div>
     </div>
