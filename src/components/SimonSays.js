35b6c47b89f67e61b66f9c89467ae45e3c298948
Isaac <isaachernandez8@gmail.com>
2023-05-23 14:48:36 -0700

    keeps track of score, can input guesses

--- src/components/SimonSays.js
+++ src/components/SimonSays.js
@@ -8,10 +8,36 @@ const SimonSays = () => {
         guesses: [],
         gameOver: true,
         highScore: 0,
-        message: `&larr;`
+        score: 0,
+        direction: ""
     })
 
-    return <div id='simon-says'>
+    function showPattern(gameStateCopy) {
+        for (let i = 0; i < gameStateCopy.pattern.length; i++) {
+            setTimeout(() => {
+                gameStateCopy.direction = gameStateCopy.pattern[i]
+                setGameState(gameStateCopy)
+            }, i * 1000 + 1000);
+        }
+    }
+
+    function inputColor(color) {
+        let gameStateCopy = {...gameState}
+        gameStateCopy.guesses.push(color)
+        let idx = gameStateCopy.guesses.lastIndexOf(color)
+        if (gameStateCopy.guesses[idx] === gameStateCopy.pattern[idx]) {
+            gameStateCopy.pattern.push(Math.floor(Math.random() * 4))
+            gameStateCopy.score++
+            setGameState(gameStateCopy)
+        } else {
+            gameStateCopy.gameOver = true
+            if (gameStateCopy.score > gameStateCopy.highScore) {gameStateCopy.highScore = gameStateCopy.score}
+            setGameState(gameStateCopy)
+        }
+    }
+
+    return <div id='simon-says' style={{width: "100%"}}>
+        <div className='row' style={{justifyContent: "space-between"}}>
         <div
         className="highlight clickable button back-button"
         onClick={() => {
@@ -20,14 +46,30 @@ const SimonSays = () => {
       >
         &lt;-
       </div>
+      <div>
+      <div>HI SCORE: {gameState.highScore}</div>
+      <div onClick={() => console.log(gameState)}>SCORE: {gameState.score}</div>
+      </div>
+      </div>
       <div id='simon-says-board' className='column'>
-        <div id='red'></div>
+        <div id='red' className='clickable' onClick={() => inputColor(0)}></div>
         <div className='row'>
-            <div id='green'></div>
-            <div id="simon-center">{gameState.message}</div>
-            <div id='blue'></div>
+            <div id='green' className='clickable' onClick={() => inputColor(3)}></div>
+            <div id="simon-center">{gameState.direction}</div>
+            <div id='blue' className='clickable' onClick={() => inputColor(1)}></div>
         </div>
-        <div id='yellow'></div>
+        <div id='yellow' className='clickable' onClick={() => inputColor(2)}></div>
+      </div>
+      <div className='row'>
+        <div className='clickable button highlight'
+        onClick={() => {
+            let gameStateCopy = {...gameState}
+            gameStateCopy.score = 0
+            gameStateCopy.pattern = [Math.floor(Math.random() * 4)]
+            gameStateCopy.gameOver = false
+            gameStateCopy.guesses = []
+            setGameState(gameStateCopy)
+        }}>NEW GAME</div>
       </div>
     </div>
 }
