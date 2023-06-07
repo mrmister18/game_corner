c376ebcaf9ac59883275513788a5864329ca4f89
Isaac <isaachernandez8@gmail.com>
2023-06-06 22:26:57 -0700

    adjusted card size, can deal cards, shows point values, dealer card and points hidden initially

--- src/components/Blackjack.js
+++ src/components/Blackjack.js
@@ -1,100 +1,180 @@
-import React, { useState, useEffect } from 'react';
-import { useNavigate } from 'react-router-dom'
-import { shuffle, deal, suit } from '../game_logic/blackjack'
+import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { pointTally, shuffle, suit } from "../game_logic/blackjack";
 
 const Blackjack = () => {
-    const navigate = useNavigate()
-    const [gameState, setGameState] = useState({
-      deck: [],
-        money: 1000,
-        dealer: {
-            hand: [],
-            points: 0
-        },
-        player: {
-            hand: [],
-            points: 0
-        },
-        gameOver: true,
-        bet: 1
-    })
-    const [dealerTurn, setDealerTurn] = useState(false)
+  const navigate = useNavigate();
+  const [gameState, setGameState] = useState({
+    deck: [],
+    money: 1000,
+    dealer: {
+      hand: [],
+      points: 0,
+    },
+    player: {
+      hand: [],
+      points: 0,
+    },
+    gameOver: true,
+    bet: 1,
+  });
+  const [dealerTurn, setDealerTurn] = useState(false);
 
-    useEffect(() => {
-      let suits = ["hearts", "diamonds", "spades", "clubs"]
-let values = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
-let deck = []
-for (let i = 0; i < suits.length; i++) {
-  for (let j = 0; j < values.length; j++) {
-    deck.push({
-      suit: suits[i],
-      value: values[j]
-    })
-  }
-}
-let gameStateCopy = {...gameState, deck: deck}
-setGameState(gameStateCopy)
-    }, [])
-    return <div id='blackjack'>
-        <div
-          className="highlight clickable button back-button"
-          onClick={() => {
-            navigate("/");
-          }}
-        >
-          &lt;-
-        </div>
-        <div className='row'>
-          <div className='column'>
-            <div className='row'>
-              {gameState.dealer.hand.map((card) => {
-              return <div className='card'><div>{card.value}</div><div className='suit'>{suit(card.suit)}</div></div>
-              })}
-            </div>
-            <div style={{textAlign: "center"}}>BLACK JACK PAYS 2 TO 1<br></br>DEALER MUST DRAW TO 16 AND STAND ON 17</div>
-            <div className='row'>
-              {gameState.player.hand.map((card) => {
-              return <div className='card'><div>{card.value}</div><div className='suit'>{suit(card.suit)}</div></div>
-              })}
-            </div>
+  useEffect(() => {
+    let suits = ["hearts", "diamonds", "spades", "clubs"];
+    let values = [
+      "2",
+      "3",
+      "4",
+      "5",
+      "6",
+      "7",
+      "8",
+      "9",
+      "10",
+      "J",
+      "Q",
+      "K",
+      "A",
+    ];
+    let deck = [];
+    for (let i = 0; i < suits.length; i++) {
+      for (let j = 0; j < values.length; j++) {
+        deck.push({
+          suit: suits[i],
+          value: values[j],
+        });
+      }
+    }
+    let gameStateCopy = { ...gameState, deck: deck };
+    setGameState(gameStateCopy);
+  }, []);
+  return (
+    <div id="blackjack">
+      <div
+        className="highlight clickable button back-button"
+        onClick={() => {
+          navigate("/");
+        }}
+      >
+        &lt;-
+      </div>
+      <div className="row">
+        <div className="column">
+        <div>{dealerTurn ? gameState.dealer.points : null}</div>
+          <div className="row">
+            {gameState.dealer.hand.map((card, idx) => {
+              return (
+                <div className="card">
+                  {idx === 0 && !dealerTurn ? null : <><div>{card.value}</div>
+                  <div className="suit">{suit(card.suit)}</div></>}
+                </div>
+              );
+            })}
           </div>
-          <div className='column'>
-            <div className={`button ${gameState.bet < 10 && gameState.gameOver ? "clickable highlight" : null}`} onClick={() => {
+          <div style={{ textAlign: "center" }}>
+            BLACK JACK PAYS 2 TO 1<br></br>DEALER MUST DRAW TO 16 AND STAND ON
+            17
+          </div>
+          <div className="row">
+            {gameState.player.hand.map((card) => {
+              return (
+                <div className="card">
+                  <div>{card.value}</div>
+                  <div className="suit">{suit(card.suit)}</div>
+                </div>
+              );
+            })}
+          </div>
+          <div>{gameState.player.points}</div>
+        </div>
+        <div className="column">
+          <div
+            className={`button ${
+              gameState.bet < 10 && gameState.gameOver
+                ? "clickable highlight"
+                : null
+            }`}
+            onClick={() => {
               if (gameState.bet < 10 && gameState.gameOver) {
-                let gameStateCopy = {...gameState}
-                gameStateCopy.bet++
-                setGameState(gameStateCopy)
+                let gameStateCopy = { ...gameState };
+                gameStateCopy.bet++;
+                setGameState(gameStateCopy);
               }
-            }}>&#9650;</div>
-            <div className='button'>{gameState.bet}</div>
-            <div className={`button ${gameState.bet > 1 && gameState.gameOver ? "clickable highlight" : null}`} onClick={() => {
-              if (gameState.bet > 1 && gameState.gameOver) {
-                let gameStateCopy = {...gameState}
-                gameStateCopy.bet--
-                setGameState(gameStateCopy)
-              }
-            }}>&#9660;</div>
-            <div className='button'>${gameState.money}</div>
-            {gameState.gameOver ? <div className='button clickable highlight' onClick={() => {
-              let gameStateCopy = {...gameState}
-              gameStateCopy.deck = shuffle(gameStateCopy.deck)
-              function initDeal(hand) {
-                let idx = Math.floor(Math.random() * gameStateCopy.deck.length)
-                hand.push(gameStateCopy.deck[idx])
-                gameStateCopy.deck.splice(idx, 1)
-                return hand
-              }
-              gameStateCopy.player.hand = initDeal(gameStateCopy.player.hand)
-              gameStateCopy.dealer.hand = initDeal(gameStateCopy.dealer.hand)
-              gameStateCopy.player.hand = initDeal(gameStateCopy.player.hand)
-              gameStateCopy.dealer.hand = initDeal(gameStateCopy.dealer.hand)
-              gameStateCopy.money -= gameStateCopy.bet
-              gameStateCopy.gameOver = false
-              setGameState(gameStateCopy)
-            }}>DEAL</div> : null}
+            }}
+          >
+            &#9650;
           </div>
+          <div className="button">{gameState.bet}</div>
+          <div
+            className={`button ${
+              gameState.bet > 1 && gameState.gameOver
+                ? "clickable highlight"
+                : null
+            }`}
+            onClick={() => {
+              if (gameState.bet > 1 && gameState.gameOver) {
+                let gameStateCopy = { ...gameState };
+                gameStateCopy.bet--;
+                setGameState(gameStateCopy);
+              }
+            }}
+          >
+            &#9660;
+          </div>
+          <div className="button">${gameState.money}</div>
+          {gameState.gameOver ? (
+            <div
+              className="button clickable highlight"
+              onClick={() => {
+                let gameStateCopy = { ...gameState };
+                gameStateCopy.deck = shuffle(gameStateCopy.deck);
+                function initDeal(hand) {
+                  hand.push(gameStateCopy.deck[0]);
+                  gameStateCopy.deck.splice(0, 1);
+                  return hand;
+                }
+                gameStateCopy.player.hand = initDeal(gameStateCopy.player.hand);
+                gameStateCopy.dealer.hand = initDeal(gameStateCopy.dealer.hand);
+                gameStateCopy.player.hand = initDeal(gameStateCopy.player.hand);
+                gameStateCopy.dealer.hand = initDeal(gameStateCopy.dealer.hand);
+                gameStateCopy.money -= gameStateCopy.bet;
+                gameStateCopy.gameOver = false;
+                gameStateCopy.player.points = pointTally(gameStateCopy.player.hand)
+                gameStateCopy.dealer.points = pointTally(gameStateCopy.dealer.hand)
+                setGameState(gameStateCopy);
+              }}
+            >
+              DEAL
+            </div>
+          ) : null}
+          {gameState.gameOver ? null : (<>
+            <div
+              className="button clickable highlight"
+              onClick={() => {
+                let gameStateCopy = { ...gameState };
+                function hit(hand) {
+                  let gameStateCopy = { ...gameState };
+                  hand.push(gameStateCopy.deck[0]);
+                  gameStateCopy.deck.splice(0, 1);
+                  return hand;
+                }
+                gameStateCopy.player.hand = hit(gameStateCopy.player.hand);
+                gameStateCopy.player.points = pointTally(gameStateCopy.player.hand)
+                setGameState(gameStateCopy)
+              }}
+            >
+              HIT
+            </div>
+            <div className="button highlight clickable" onClick={() => {
+              setDealerTurn(true)
+            }}>STAND</div>
+            </>
+          )}
         </div>
+      </div>
     </div>
-}
+  );
+};
 
 export default Blackjack;
--- src/game_logic/blackjack.js
+++ src/game_logic/blackjack.js
@@ -14,16 +14,24 @@ export function shuffle(array) {
   return array;
 }
 
-export function deal(hand) {
-    let idx = Math.floor(Math.random() * deck.length)
-    hand.push(deck[idx])
-    deck.splice(idx, 1)
-    return hand
-  }
-
 export function suit(suit) {
     if (suit === "spades") {return "\u2660"}
     else if (suit === "clubs") {return "\u2663"}
     else if (suit === "hearts") {return "\u2665"}
     else {return "\u2666"}
+}
+
+export function pointTally(hand) {
+    hand.sort((a, _) => {
+        if (a.value === "A") {return 1}
+      })
+    let points = 0
+    let alt = 0
+    for (let i = 0; i < hand.length; i++) {
+        if (hand[i].value === "A") {alt = points + 11
+        points += 1} else if (hand[i].value === "J" || hand[i].value === "Q" || hand[i].value === "K") {
+            points += 10
+        } else {points += Number(hand[i].value)}
+    } if (alt > points && alt < 22) {return alt}
+    return points
 }
